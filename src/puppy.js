5039812dadd012d632ec5ed6c28e358101b4f4ba
Jack Mason <jack.mason1@gmail.com>
2021-04-17 14:21:00 -0400

    Added puppy.js

---
+++ src/puppy.js
@@ -0,0 +1,18 @@
+# src/puppy.js
+
+class Puppy {
+    constructor(name, breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    speak() {
+        console.log(`${name} says 'arf'`);
+    }
+
+    about() {
+        console.log(`${name} is just a lil pup. He's a ${breed}`);
+    }
+}
+
+module.exports = Puppy;
